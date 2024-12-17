4ff4dd910d3dba12f390b1bbd6945d154ac2dfe5
daisuke <daisuke.zr@icloud.com>
2024-12-17 17:38:57 -0600

    question 1 (1) (2) answered

---
+++ Lab10/question1/src/main/java/org/example/bugreporter/Main.java
@@ -0,0 +1,11 @@
+package org.example.bugreporter;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BugReportGenerator brg = new BugReportGenerator();
+		brg.reportGenerator();
+
+	}
+
+}
---
+++ Lab10/question1/src/main/java/org/example/javapackage/ClosedCurve.java
@@ -0,0 +1,8 @@
+package org.example.javapackage;
+
+import org.example.bugreporter.BugReport;
+
+@BugReport(assignedTo="Tom Jones", severity=1, reportedBy="Corazza")
+public interface ClosedCurve {
+	public double computePerimeter();
+}
